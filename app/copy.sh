26e29f4a4ff2d63cc2ed4519941ad4255b42ed28
CBuiVNG <info@iloki.nl>
2020-06-18 20:49:46 +0200

    cleanups

--- app/copy.sh
+++
@@ -1,4 +0,0 @@
-cp ../../classification/ML/Middle_model.pkl main_model.pkl 
-cp ../../classification/ML/Middle_Sub_model.pkl sub_model.pkl
-cp ../../classification/ML/Middle_labels.pkl main_slugs.pkl
-cp ../../classification/ML/Middle_Sub_labels.pkl sub_slugs.pkl
