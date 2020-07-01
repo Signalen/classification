302d57e02cc181efff745473eb4a21f20592d746
CBuiVNG <info@iloki.nl>
2020-07-01 12:04:07 +0200

    adjusted default signals url

--- app/settings.py
+++ app/settings.py
@@ -1,3 +1,3 @@
 import os
 
-SIGNALS_CATEGORY_URL = os.getenv('SIGNALS_CATEGORY_URL', 'https://api.signalen.s-hertogenbosch.dev/signals/v1/public/terms')
+SIGNALS_CATEGORY_URL = os.getenv('SIGNALS_CATEGORY_URL', 'https://backend.signalen.demoground.nl/signals/v1/public/terms')
